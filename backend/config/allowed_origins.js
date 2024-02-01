26e7b990236db0194c72cb79f3296a958cd3a17c
Khoi Nguyen <nminhkhoi0818@gmail.com>
2024-02-01 17:27:53 +0700

    fix cors again

--- backend/config/allowed_origins.js
+++ backend/config/allowed_origins.js
@@ -1,7 +1,7 @@
 const origins = [
   "http://127.0.0.1:5173",
   "http://localhost:5173",
-  "https://quizzapppp.onrender.com/",
+  "https://quizzapppp.onrender.com",
 ];
 
 module.exports = origins;
